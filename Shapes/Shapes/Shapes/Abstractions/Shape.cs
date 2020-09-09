491ab3a1d541e943af017210e834d035199a5dc7
Josh Kunz <jkunz@menard-inc.com>
2020-09-09 10:34:54 -0500

    removed an extra Shapes directory

--- Shapes/Shapes/Shapes/Abstractions/Shape.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shapes.Abstractions
-{
-    public abstract class Shape
-    {
-        public abstract float surfaceArea();
-        public abstract float volume();
-        public abstract void render();
-    }
-}
